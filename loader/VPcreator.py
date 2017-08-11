a401a27eca325f311eda570bf30fdba3add39ad3
matteuan <elcossu@gmail.com>
2017-08-11 16:35:16 +0200

    additional jar in loader

--- loader/VPcreator.py
+++ loader/VPcreator.py
@@ -1,4 +1,4 @@
-import sys, re, argparse
+import sys, re, argparse, subprocess, os
 from pyspark import SparkContext
 from pyspark.sql import HiveContext
 from pyspark.sql import DataFrameWriter
@@ -47,6 +47,8 @@ class VP_creator:
         self.statsFile = statsFile
         self.statsEnabled = statsFile != ""
         self.stats = Stats()
+        self.property_table_jar = ""
+        self.property_table_enabled = False
         
         # from now on, use the proper DB
         sqlContext.sql("CREATE DATABASE IF NOT EXISTS " + self.outputDB)
@@ -88,24 +90,42 @@ class VP_creator:
             with open(self.statsFile, "w") as f:
                 f.write(stat.getSerializedStats())
         print "Statistics created: %d / %d "  % (i, total_properties)
+    
+    def set_propertytable_jar(self, PTjar):
+        self.property_table_enabled = True
+        self.property_table_jar = PTjar
+    
+    def create_property_table(self):
+        # TODO set the thing only with python
+        translate_command =  'spark2-submit --driver-memory 3G --jars ../executor/commons-cli-1.3.1.jar  --conf "spark.driver.userClassPathFirst=true" --class Main ' + self.property_table_jar + " -i " + self.inputFile + " -o " + self.outputDB
+        print translate_command
+        execution_output = subprocess.check_output(['bash','-c', translate_command]) 
+        print execution_output
+        print "Property table loaded"
+
 
     def run_creator(self):
         self.create_triple_table()
         self.extract_properties()
         self.create_VP_tables()
+        if(self.property_table_enabled):
+            self.create_property_table()
 
 def main():
     parser = argparse.ArgumentParser(description='Load a RDF into the Hive Mestastore using Vertical Partitioning.')
     parser.add_argument('input', metavar='input path', help='The HDFS path of the input RDF file')
     parser.add_argument('output', metavar='output database', help='The name of the database where to load the data. ')
     parser.add_argument('-s','-stats', metavar='[output stats file]', help='Statistics are computed and saved in the file.', default='')
+    parser.add_argument('-p','-propertytable', metavar='[property table jar]', help='If loaded with property table, the jar of the separate loader as input.', default='')
     
     args = parser.parse_args()
     
-    sc = SparkContext(appName="GYM_loader")
+    sc = SparkContext(appName="SparkVP_loader")
     sqlContext = HiveContext(sc)
    
-    creator = VP_creator(sc, sqlContext, args.input, args.output, args.stats)
+    creator = VP_creator(sc, sqlContext, args.input, args.output, args.s)
+    if(args.p):
+        creator.set_propertytable_jar(args.p)
     creator.run_creator()
 
 if __name__ == "__main__":
